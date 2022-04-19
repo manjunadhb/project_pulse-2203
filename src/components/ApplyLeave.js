02aa55801aacb33f200af1dab690271a732d56b5
chaitanyakrishna <chaitanya21996@gmail.com>
2022-04-19 12:03:51 +0530

    removed unwanted data from leave page

--- src/components/ApplyLeave.js
+++ src/components/ApplyLeave.js
@@ -4,15 +4,10 @@ function ApplyLeave() {
   return (
     <div>
 
-    <h1>Create a Apply Leave page</h1>
-    <h2>Assigned into Chaithanya</h2>
+    <h1> Apply Leave</h1>
+    <h3>choose leave type and dates to apply for leave</h3>
 
-    <ul>
-        <li>Form filling</li>
-        <li>Validation</li>
-        <li>Submission to server</li>
-      </ul>
-    
+   
     
   </div>
   )
