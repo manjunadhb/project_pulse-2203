fd7cde891a732eda52aecb8236050ba6808b6912
MD ABDUL NADEEM <manadeem36@gmail.com>
2022-04-23 11:49:26 +0530

    Ranking page updated

--- src/components/Rankings.js
+++ src/components/Rankings.js
@@ -1,23 +1,233 @@
-import React from 'react'
+import React from "react";
+
+import { Button, Accordion, Container,Table } from "react-bootstrap";
+import "bootstrap/dist/css/bootstrap.min.css";
 
 function Rankings() {
   return (
-    <div>
+    <>
+      <Container className="subMenu">
+        <span>Report for</span>
 
-      <h1>Create a Ranking page</h1>
-      <h2>Assigned into Nadeem</h2>
+        <select className="OptwithSearch">
+          <option></option>
+          <option>2021</option>
+          <option>2020</option>
+          <option>2019</option>
+          <option>2018</option>
+        </select>
+        <Button variant="primary" className="Report">
+          Report
+        </Button>
+      </Container>
 
+      <Container fluid>
+        <Accordion defaultActiveKey="0">
+          <Accordion.Item eventKey="0">
+            <Accordion.Header>Accordion Item #1</Accordion.Header>
+            <Accordion.Body>
+              <select className="p-2">
+                <option>All</option>
+                <option>2</option>
+                <option>3</option>
+                <option>4</option>
+                <option>5</option>
+              </select>
+              <span>recods</span>
 
-      <ul>
-        <li>Send and Recieve data from server</li>
-        <li>Populate data in table</li>
-        <li>search functionaity in table</li>
-        <li>Link to show user dashboard page</li>
-        <li>Generate Receipt</li>
-      </ul>
-      
-    </div>
-  )
+              <input
+                type="Search"
+                className="search"
+                placeholder="Search Here..."
+              />
+              <span className="search">Search:</span>
+              <Table striped bordered hover >
+                <thead>
+                  <tr>
+                    <th >Rank</th>
+                    <th >Student Name</th>
+                    <th >Student ID</th>
+                    <th >Late to Office</th>
+                    <th >Min.Hrs Missed</th>
+                    <th >Disable Reason</th>
+                    <th >Max.Score</th>
+                    <th >Score</th>
+                    <th >Performance</th>
+                    <th >Last Attended</th>
+                    <th >Total Days</th>
+                    <th >Working Days</th>
+                    <th >Leaves</th>
+                    <th >Absent</th>
+                    <th >Days Attended</th>
+                  </tr>
+                </thead>
+                <tbody>
+                <tr >
+                    <td >DATA</td>
+                    <td >DATA</td>
+                    <td >DATA</td>
+                    <td >DATA</td>
+                    <td >DATA</td>
+                    <td >DATA</td>
+                    <td >DATA</td>
+                    <td >DATA</td>
+                    <td >DATA</td>
+                    <td >DATA</td>
+                    <td >DATA</td>
+                    <td >DATA</td>
+                    <td >DATA</td>
+                    <td >DATA</td>
+                    <td >DATA</td>
+                  </tr>
+
+                  <tr >
+                    <td >DATA</td>
+                    <td >DATA</td>
+                    <td >DATA</td>
+                    <td >DATA</td>
+                    <td >DATA</td>
+                    <td >DATA</td>
+                    <td >DATA</td>
+                    <td >DATA</td>
+                    <td >DATA</td>
+                    <td >DATA</td>
+                    <td >DATA</td>
+                    <td >DATA</td>
+                    <td >DATA</td>
+                    <td >DATA</td>
+                    <td >DATA</td>
+                  </tr>  
+                  <tr >
+                    <td >DATA</td>
+                    <td >DATA</td>
+                    <td >DATA</td>
+                    <td >DATA</td>
+                    <td >DATA</td>
+                    <td >DATA</td>
+                    <td >DATA</td>
+                    <td >DATA</td>
+                    <td >DATA</td>
+                    <td >DATA</td>
+                    <td >DATA</td>
+                    <td >DATA</td>
+                    <td >DATA</td>
+                    <td >DATA</td>
+                    <td >DATA</td>
+                  </tr>
+                </tbody>
+              </Table>
+
+              
+              
+            </Accordion.Body>
+          </Accordion.Item>
+        </Accordion>
+      </Container>
+
+      <Container fluid>
+        <Accordion defaultActiveKey="0">
+          <Accordion.Item eventKey="0">
+            <Accordion.Header>Accordion Item #1</Accordion.Header>
+            <Accordion.Body>
+              <select className="p-2">
+                <option>All</option>
+                <option>2</option>
+                <option>3</option>
+                <option>4</option>
+                <option>5</option>
+              </select>
+              <span>Recods</span>
+
+              <input
+                type="Search"
+                className="search"
+                placeholder="Search Here..."
+              />
+              <span className="search">Search:</span>
+              <Table striped bordered hover >
+                <thead>
+                  <tr>
+                    <th >Rank</th>
+                    <th >Student Name</th>
+                    <th >Student ID</th>
+                    <th >Late to Office</th>
+                    <th >Min.Hrs Missed</th>
+                    <th >Disable Reason</th>
+                    <th >Max.Score</th>
+                    <th >Score</th>
+                    <th >Performance</th>
+                    <th >Last Attended</th>
+                    <th >Total Days</th>
+                    <th >Working Days</th>
+                    <th >Leaves</th>
+                    <th >Absent</th>
+                    <th >Days Attended</th>
+                  </tr>
+                </thead>
+                <tbody>
+                <tr >
+                    <td >DATA</td>
+                    <td >DATA</td>
+                    <td >DATA</td>
+                    <td >DATA</td>
+                    <td >DATA</td>
+                    <td >DATA</td>
+                    <td >DATA</td>
+                    <td >DATA</td>
+                    <td >DATA</td>
+                    <td >DATA</td>
+                    <td >DATA</td>
+                    <td >DATA</td>
+                    <td >DATA</td>
+                    <td >DATA</td>
+                    <td >DATA</td>
+                  </tr>
+
+                  <tr >
+                    <td >DATA</td>
+                    <td >DATA</td>
+                    <td >DATA</td>
+                    <td >DATA</td>
+                    <td >DATA</td>
+                    <td >DATA</td>
+                    <td >DATA</td>
+                    <td >DATA</td>
+                    <td >DATA</td>
+                    <td >DATA</td>
+                    <td >DATA</td>
+                    <td >DATA</td>
+                    <td >DATA</td>
+                    <td >DATA</td>
+                    <td >DATA</td>
+                  </tr>  
+                  <tr >
+                    <td >DATA</td>
+                    <td >DATA</td>
+                    <td >DATA</td>
+                    <td >DATA</td>
+                    <td >DATA</td>
+                    <td >DATA</td>
+                    <td >DATA</td>
+                    <td >DATA</td>
+                    <td >DATA</td>
+                    <td >DATA</td>
+                    <td >DATA</td>
+                    <td >DATA</td>
+                    <td >DATA</td>
+                    <td >DATA</td>
+                    <td >DATA</td>
+                  </tr>
+                </tbody>
+              </Table>
+
+              
+              
+            </Accordion.Body>
+          </Accordion.Item>
+        </Accordion>
+      </Container>
+    </>
+  );
 }
 
-export default Rankings
+export default Rankings;
